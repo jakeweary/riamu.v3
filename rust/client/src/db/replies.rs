69073b460ea84e00d524b4cce46cc1d61dfea58d
me <literally@me>
2023-11-24 16:42:39 +0300

    kek

--- rust/client/src/db/replies.rs
+++ rust/client/src/db/replies.rs
@@ -1,25 +1,6 @@
-use rand::prelude::*;
-
 use super::*;
 
 pub async fn random_reply(pool: &Pool) -> sqlx::Result<Option<String>> {
-  let mut tx = pool.begin().await?;
-
-  let q = sqlx::query_scalar("select count(*) from replies");
-  let count = q.fetch_one(&mut *tx).await?;
-
-  let reply = match count {
-    0 => None,
-    n => {
-      let q = sqlx::query_scalar("select reply from replies limit 1 offset ?");
-      let offset = thread_rng().gen_range(0_i64..n);
-      let reply = q.bind(offset).fetch_one(&mut *tx).await?;
-
-      Some(reply)
-    }
-  };
-
-  tx.commit().await?;
-
-  Ok(reply)
+  let q = sqlx::query_scalar("select reply from replies order by random() limit 1");
+  q.fetch_optional(pool).await
 }
