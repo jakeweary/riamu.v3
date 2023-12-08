9e701f4f4f9eae0bea0bce4960f9c64415ba4dbd
me <literally@me>
2023-12-08 18:22:10 +0300

    feat(weather): daily forecast improvements

--- rust/lib/src/weather/render/components/daily.rs
+++ rust/lib/src/weather/render/components/daily.rs
@@ -1,6 +1,7 @@
 use cairo::*;
 
 use super::fmt::Num;
+use super::range::Range;
 use super::Result;
 use super::*;
 
@@ -8,15 +9,53 @@ pub fn daily(ctx: &Context, weather: &api::Onecall) -> Result<()> {
   let width = 8.0 * 28.0;
   let day_w = width / weather.daily.len() as f64;
 
+  let temp_min = Range::of(&weather.daily, |d| d.temp.min);
+  let temp_max = Range::of(&weather.daily, |d| d.temp.max);
+  let temp = temp_min & temp_max;
+
+  let wind_speed = Range::of(&weather.daily, |d| d.wind_speed);
+  let wind_gust = Range::of(&weather.daily, |d| d.wind_gust);
+  let wind = wind_speed & wind_gust & 0.0;
+
   ctx.save()?;
   ctx.translate(285.0, 8.0);
 
+  let font_size = 10.0;
+  util::cairo::set_font_variations(ctx, "opsz=10,wdth=50")?;
+  ctx.set_font_size(font_size);
+
+  let number = |color, n: String| {
+    ctx.translate(0.0, font_size);
+    ctx.move_to(0.0, 0.0);
+    util::cairo::center_text_by_template(ctx, "0", &n)?;
+    ctx.set_source_rgb_u32(color);
+    ctx.show_text(&n)
+  };
+
+  let indicator = |r: Range, min, max| {
+    let h = 3.0 - 2.0 * font_size;
+
+    ctx.save()?;
+    ctx.translate(8.0, 0.0);
+    ctx.set_line_cap(LineCap::Round);
+
+    ctx.move_to(0.0, 0.0);
+    ctx.line_to(0.0, h);
+    ctx.set_source_rgb_u32(0x2b2d31);
+    ctx.set_line_width(4.0);
+    ctx.stroke()?;
+
+    ctx.move_to(0.0, h * r.unlerp(min));
+    ctx.line_to(0.0, h * r.unlerp(max));
+    ctx.set_source_rgb_u32(0x949ba4);
+    ctx.set_line_width(2.0);
+    ctx.stroke()?;
+
+    ctx.restore()
+  };
+
   for day in &weather.daily {
     ctx.save()?;
-
-    let font_size = 10.0;
-    util::cairo::set_font_variations(ctx, "opsz=10,wdth=50")?;
-    ctx.set_font_size(font_size);
 
     let dt = util::datetime(weather.timezone_offset, day.dt);
     let text = dt.format("%a").to_string();
@@ -34,38 +73,18 @@ pub fn daily(ctx: &Context, weather: &api::Onecall) -> Result<()> {
     ctx.restore()?;
     ctx.translate(0.0, 28.0 + 1.0);
 
-    let text = format!("{:.0}°", Num(day.temp.max));
-    ctx.move_to(0.0, font_size);
-    util::cairo::center_text_by_template(ctx, "°0°", &text)?; // two ° is a hack for proper alignment
-    ctx.set_source_rgb_u32(0xffffff);
-    ctx.show_text(&text)?;
-    ctx.translate(0.0, font_size);
-
-    let text = format!("{:.0}°", Num(day.temp.min));
-    ctx.move_to(0.0, font_size);
-    util::cairo::center_text_by_template(ctx, "°0°", &text)?; // two ° is a hack for proper alignment
-    ctx.set_source_rgb_u32(0x949ba4);
-    ctx.show_text(&text)?;
-    ctx.translate(0.0, font_size);
+    number(0xffffff, format!("{:.0}", Num(day.temp.max)))?;
+    number(0x949ba4, format!("{:.0}", Num(day.temp.min)))?;
+    indicator(temp, day.temp.min, day.temp.max)?;
 
     draw::arrow(ctx, 0.0, 13.5, 6.0, (day.wind_deg as f64).to_radians());
     ctx.set_source_rgb_u32(0xffffff);
     ctx.fill()?;
     ctx.translate(0.0, 25.0);
 
-    let text = format!("{}", Num(day.wind_speed));
-    ctx.move_to(0.0, font_size);
-    util::cairo::center_text_by_template(ctx, "0", &text)?;
-    ctx.set_source_rgb_u32(0xffffff);
-    ctx.show_text(&text)?;
-    ctx.translate(0.0, font_size);
-
-    let text = format!("{}", Num(day.wind_gust));
-    ctx.move_to(0.0, font_size);
-    util::cairo::center_text_by_template(ctx, "0", &text)?;
-    ctx.set_source_rgb_u32(0x949ba4);
-    ctx.show_text(&text)?;
-    ctx.translate(0.0, font_size);
+    number(0xffffff, format!("{:.0}", day.wind_gust))?;
+    number(0x949ba4, format!("{:.0}", day.wind_speed))?;
+    indicator(wind, day.wind_speed, day.wind_gust)?;
 
     ctx.restore()?;
     ctx.translate(day_w, 0.0);
